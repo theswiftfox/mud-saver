fd877916d532bd01aef10874282dc02e0889669b
Patrick Gantner <patrick.gantner@linesofcode.at>
2020-05-11 20:17:25 +0200

    web-ui: initial setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+extern crate rocket_contrib;
+
+use rocket_contrib::templates::Template;
+
+mod ui;
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount(
+        "/", 
+        routes![ui::index]
+    ).attach(Template::fairing())
+    .launch();
 }
