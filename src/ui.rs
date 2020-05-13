84a0b790adfd5e9f2d6afb4896009b7f3da6930c
Elena Gantner <elena.gantner@gmail.com>
2020-05-13 14:59:19 +0200

    #9 - use partial templates to reduce duplicate code

--- src/ui.rs
+++ src/ui.rs
@@ -3,14 +3,14 @@ use rocket_contrib::templates::Template;
 
 #[get("/")]
 pub fn index() -> Result<Template, Status> {
-    Ok(Template::render("index", ()))
+    Ok(Template::render("base", "index"))
 }
 
 #[get("/mud-runner")]
 pub fn mud_runner() -> Result<Template, Status> {
-    Ok(Template::render("mudrunner", ()))
+    Ok(Template::render("base", "mudrunner"))
 }
 #[get("/snow-runner")]
 pub fn snow_runner() -> Result<Template, Status> {
-    Ok(Template::render("snowrunner", ()))
+    Ok(Template::render("base", "snowrunner"))
 }
