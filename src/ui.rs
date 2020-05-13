b2095fb2c72d58aa8beb0e9b160008d985b17a28
Elena Gantner <elena.gantner@gmail.com>
2020-05-13 14:23:25 +0200

    #9 - demo page

--- src/ui.rs
+++ src/ui.rs
@@ -5,3 +5,12 @@ use rocket_contrib::templates::Template;
 pub fn index() -> Result<Template, Status> {
     Ok(Template::render("index", ()))
 }
+
+#[get("/mud-runner")]
+pub fn mud_runner() -> Result<Template, Status> {
+    Ok(Template::render("mudrunner", ()))
+}
+#[get("/snow-runner")]
+pub fn snow_runner() -> Result<Template, Status> {
+    Ok(Template::render("snowrunner", ()))
+}
