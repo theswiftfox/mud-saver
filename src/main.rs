3e5131719da577bba9dea5ec784acd05008ca2e6
Patrick Gantner <patrick.gantner@linesofcode.at>
2020-05-13 17:38:32 +0200

    #9 - added option to run with --with-ui, which will launch a window with a web-view using the web-view package

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@ extern crate rocket_contrib;
 use rocket_contrib::templates::Template;
 use rocket_contrib::serve::StaticFiles;
 
+use std::thread;
+
 mod ui;
 
-fn main() {
+fn start_rocket() {
     rocket::ignite()
     .mount(
         "/", 
@@ -22,4 +24,44 @@ fn main() {
     .mount("/images", StaticFiles::from("./images"))
     .attach(Template::fairing())
     .launch();
+}
+
+fn start_ui() {
+    web_view::builder()
+		.title("MudSaver")
+		.content(web_view::Content::Url("http://localhost:8000"))
+		.size(800, 600)
+		.resizable(true)
+		.debug(true)
+		.user_data(())
+        .invoke_handler(|_webview, _arg| Ok(()))
+        .run()
+        .unwrap();
+}
+
+fn with_ui() {
+    let _ = thread::spawn(|| {
+        start_rocket()
+    });
+    thread::sleep(std::time::Duration::from_secs(1));
+    start_ui();
+
+    std::process::exit(0);
+}
+
+fn headless() {
+    start_rocket();
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 {
+        if &args[1] == "--with-ui" {
+            with_ui()
+        } else {
+            println!("argument not recognized")
+        }
+    } else {
+        headless()
+    }
 }
