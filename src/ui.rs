024c8e333b589f2d03eae97bb4eebdebcbe9642c
Elena Gantner <elena.gantner@gmail.com>
2020-05-14 13:24:50 +0200

    #9 - playing around with html & javascript for responsive frontend

--- src/ui.rs
+++ src/ui.rs
@@ -3,14 +3,19 @@ use rocket_contrib::templates::Template;
 
 #[get("/")]
 pub fn index() -> Result<Template, Status> {
-    Ok(Template::render("base", "index"))
+    Ok(Template::render("base", ()))
+}
+
+#[get("/overview")]
+pub fn overview() -> Result<Template, Status> {
+    Ok(Template::render("index", ()))
 }
 
 #[get("/mud-runner")]
 pub fn mud_runner() -> Result<Template, Status> {
-    Ok(Template::render("base", "mudrunner"))
+    Ok(Template::render("mudrunner", ()))
 }
 #[get("/snow-runner")]
 pub fn snow_runner() -> Result<Template, Status> {
-    Ok(Template::render("base", "snowrunner"))
+    Ok(Template::render("snowrunner", ()))
 }
