5372a5b3d3b7e9d83bb6ba5078b24111538ec9d1
Thomas Vauti-Rienößl <43434564+ThomCoder@users.noreply.github.com>
2020-05-22 19:31:06 +0200

    Add skeleton code for mud_runner_saves module
    This skeleton code shall be used to review the interface.

---
+++ src/mud_runner_saves.rs
@@ -0,0 +1,26 @@
+pub struct Mudrunner_Save<'a> {
+    user_name: &'a str,
+    file_hash: u64,
+    original_name: &'a str,
+}
+
+// unction to get a vector of the mudrunner savegames' titles/user names in our app's storage
+pub fn get_archived_mudrunner_saves<'a>() -> Vec<&'a str> {
+    vec![""]
+}
+
+
+// function to get a vector of the mudrunner savegames' file names in Mudrunner's storage
+pub fn get_available_mudrunner_saves<'a>() -> Vec<&'a str> {
+    vec![""]
+}
+
+// function to archive a specific savegame to our app's storage
+pub fn archive_savegame(savegame: &Mudrunner_Save) -> Result<(), std::io::ErrorKind> {
+    Err(std::io::ErrorKind::PermissionDenied)
+}
+
+// function to install a specific savegame (overwriting the existing one)
+pub fn install_savegame(savegame: &Mudrunner_Save) -> Result<(), std::io::ErrorKind> {
+    Err(std::io::ErrorKind::PermissionDenied)
+}
