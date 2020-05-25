95592fcd2282259a5db93256b67c5509879495d8
Thomas Vauti-Rienößl <43434564+ThomCoder@users.noreply.github.com>
2020-05-25 17:50:27 +0200

    Adapt dummy functions to use AppError returns

--- src/mud_runner_saves.rs
+++ src/mud_runner_saves.rs
@@ -11,21 +11,20 @@ pub struct MudrunnerSave {
 
 // unction to get a vector of the mudrunner savegames' titles/user names in our app's storage
 pub fn get_archived_mudrunner_saves() -> Result<Vec<MudrunnerSave> , AppError> {
-    Err(AppError)
+    Err(AppError::SettingsNotFound(String::from("")))
 }
-
 
 // function to get a vector of the mudrunner savegames' file names in Mudrunner's storage
 pub fn get_available_mudrunner_saves() -> Result<Vec<MudrunnerSave> , AppError> {
-    Err(AppError)
+    Err(AppError::SettingsNotFound(String::from("")))
 }
 
 // function to archive a specific savegame to our app's storage
 pub fn archive_savegame(savegame: &MudrunnerSave) -> Result<(), AppError> {
-    Err(std::io::ErrorKind::PermissionDenied)
+    Err(AppError::SettingsNotFound(String::from("")))
 }
 
 // function to install a specific savegame (overwriting the existing one)
 pub fn install_savegame(savegame: &MudrunnerSave) -> Result<(), AppError> {
-    Err(std::io::ErrorKind::PermissionDenied)
+    Err(AppError::SettingsNotFound(String::from("")))
 }
