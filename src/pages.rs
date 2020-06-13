5c1f2be36f05b7148bb35b5fda16fac4ec93854d
Thomas Vauti-Rienößl <43434564+ThomCoder@users.noreply.github.com>
2020-06-13 22:30:08 +0200

    Issue #4: Implement index and backup of mudrunner saves
    Signed-off-by: Thomas Vauti-Rienößl <43434564+ThomCoder@users.noreply.github.com>

--- src/pages.rs
+++ src/pages.rs
@@ -4,6 +4,7 @@ use rocket_contrib::templates::Template;
 
 use crate::appconfig::Settings;
 use crate::error::AppError;
+use crate::mud_runner_saves::MudrunnerSave;
 use crate::snowrunner::SnowRunnerProfile;
 use crate::SETTINGS;
 
@@ -32,8 +33,20 @@ pub fn overview() -> Result<Template, Status> {
 }
 
 #[get("/mud-runner")]
-pub fn mud_runner() -> Result<Template, Status> {
-    Ok(Template::render("mudrunner", ()))
+pub fn mud_runner() -> Result<Template, AppError> {
+    let avail_saves = MudrunnerSave::get_available_mudrunner_saves()?;
+    Ok(Template::render("mudrunner", avail_saves))
+}
+
+#[post("/mud-runner/save?<original_name>&<user_name>")]
+pub fn store_mudrunner_save(original_name: Option<String>, user_name: Option<String>) -> Result<(), AppError> {
+    if original_name.is_none() {
+        return Err(AppError::MissingParameter(String::from("original_name")));
+    }
+    if user_name.is_none() {
+        return Err(AppError::MissingParameter(String::from("user_name")));
+    }
+    MudrunnerSave::archive_savegame(&user_name.unwrap(), &original_name.unwrap())
 }
 
 /* *** SNOW RUNNER *** */
